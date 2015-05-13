12627af4df4dc525a0f7ec48b571d8f0d06c8fef
Lee Baker <lee@leebaker.com>
2015-05-13 10:55:49 -0500

    Initial draft

---
+++ baseball-reference-highlight-favorite-team.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Highlight favorite team in baseball-reference
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       Lee Baker
+// @match        *://*.baseball-reference.com/*
+// @grant        none
+// @require    http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// ==/UserScript==
+$("a:contains('CHC')").parents("tr").css('background','#3399FF');
+
+
