abb0d32baffe77ed4bacc6c2fe68c3a705457b46
Lee Baker <lee@leebaker.com>
2015-07-13 11:11:15 -0500

    Exclude for player pages

--- baseball-reference-highlight-favorite-team.js
+++ baseball-reference-highlight-favorite-team.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Baseball Reference Highlighter
+// @name         Highlight favorite team in baseball-reference
 // @namespace    http://your.homepage/
 // @version      0.1
-// @description  Highlights players from your favorite team on Baseball Reference
+// @description  enter something useful
 // @author       Lee Baker
 // @match        *://*.baseball-reference.com/*
+// @exclude      *://*.baseball-reference.com/players/*
 // @grant        none
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // ==/UserScript==
 $("a:contains('CHC')").parents("tr").css('background','#3399FF');
-
 
